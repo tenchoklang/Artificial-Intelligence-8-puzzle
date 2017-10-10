2636daf76b4e0cc0462f262c04d60abab84f4d42
Tenzin Choklang <tenchoklang@gmail.com>
2017-10-09 23:46:00 -0400

    eighth commit v9

--- Board.java
+++ Board.java
@@ -1,9 +1,7 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
-/*
- */
-
-public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
+public class Board extends HashMap<Integer,Board> implements Comparable<Board>{
 	
 	public static final int DEFAULT_BOARD_SIZEX = 3;
 	public static final int DEFAULT_BOARD_SIZEY = 3;
@@ -11,9 +9,13 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 	public static int boardSizeX;
 	public static int boardSizeY;
 	
-	public int misplacedTiles;
+	private int[][] board;
 	
-	public int manhattanDistance;
+	
+	public String[][] misplacedBoard;
+	public int misplacedTiles =0;
+	
+	public int manhattanDistance = 0;
 	
 	public int heuristic;
 	
@@ -26,11 +28,10 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 		{8,0,4},
 		{7,6,5}
 	};
-	
 	public static boolean goalStatus = false;
 	public static Board answerBoard;//the final answer that an algorithm has found, so we can backtrack the path
 	
-	public Board parentBoard;//helps with backtracking the path taken
+	public Board parentBoard;
 	
 	public int level = 0;
 	
@@ -42,15 +43,25 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 	public Board(int[][] board, int boardSizeX, int boardSizeY){//main constructor	
 		Board.boardSizeX = boardSizeX;
 		Board.boardSizeY = boardSizeY;
+		this.board = board;
+		misplacedBoard = new String[boardSizeY][boardSizeX];
 		start_state = new int[boardSizeY][boardSizeX];
-		copyBoard(board);
-		findBlankPos();
+		current_state = new int[boardSizeY][boardSizeX];
+		initializeBoard(board);
 	}
 	
 	public Board(int[][] board){
 		this(board, DEFAULT_BOARD_SIZEX,DEFAULT_BOARD_SIZEY);//calls main
 	}
 	
+	
+	private void initializeBoard(int[][] board){
+		if(board.length != boardSizeY || board[0].length != boardSizeX){//board size required if not default
+			throw new IllegalArgumentException("Size of the board is required");
+		}
+		copyBoards();
+		findBlankPos();		
+	}
 	
 	private void checkGoalStatus(){
 		if(misplacedTiles == 0){
@@ -70,10 +81,11 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 		}
 	}
 	
-	private void copyBoard(int[][] board){//copy by value
+	private void copyBoards(){//copy by value
 		for(int y=0;y<boardSizeY;y++){
 			for(int x=0;x<boardSizeX;x++){
 				start_state[y][x] = board[y][x];
+				current_state[y][x] = board[y][x];
 			}
 		}
 	}
@@ -93,48 +105,31 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 		for(int y =0; y<boardSizeY;y++){	
 			for(int x=0;x<boardSizeX;x++){
 				if(GOAL_STATE[y][x] == start_state[y][x] || start_state[y][x] ==0){//if same or blank dont change anything
-
+					int value = start_state[y][x];
+					misplacedBoard[y][x] = Integer.toString(value);
 				}else{
+					misplacedBoard[y][x] = "X";
 					misplacedTiles++;
 				}
 			}
 		}
-		checkGoalStatus();//checks if this is the goal state after making the move
+		checkGoalStatus();
 		return misplacedTiles;
 	}	
 	
-	public int calculateHeuristic(String algorithm){//gets the Heuristic of the specified algorithm
-		
-		algorithm.toLowerCase();
-		switch(algorithm){
-		
-		case "astar misplaced":
-			level = parentBoard.level + 1;//update the level
-			heuristic = level + misplacedTiles;
-			break;
-			
-		case "astar manhattan":
-			level = parentBoard.level + 1;//update the level
-			System.out.println(findManhattanDistance(start_state, GOAL_STATE));
-			heuristic = level + findManhattanDistance(start_state, GOAL_STATE);
-			break;
-			
-		case "astar ida":
-			level = parentBoard.level + 1;
-			break;
-		
-		case "dfbnb":
-		
-			break;
-			
-		default: 
-			System.out.println("Algorithm not found");
-			
-		}
-		
-		return heuristic;
-	}
 
+//	public int[][] getBoard() {
+//		return board;
+//	}
+//
+//	public void setBoard(int[][] board) {
+//		this.board = board;
+//	}
+//
+//	public static int[][] getGoalState() {
+//		return GOAL_STATE;
+//	}
+//	
 	
 	public static void displayMisplacedBoard(String board[][]){
 		
@@ -146,99 +141,18 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 		}
 	}
 	
-
-	public static int findManhattanDistance(int[][] easyBoard, int[][] GOAL_STATE){
-		
-		int totalDistance = 0;
-		for(int y=0;y<3;y++){
-			for(int x=0;x<3;x++){
-				
-				if(easyBoard[y][x] != 0){
-					int number = easyBoard[y][x];
-					/*
-					 * Finds the Number in the GOAL_STATE that you know the coordinate to in the Board
-					 */
-					int[] coordinates = findNumber(GOAL_STATE, number);
-					int XPositionInGoalState = coordinates[1];
-					int YPositionInGoalState = coordinates[0];
-					
-					int verticalDistance = findDistance(x,XPositionInGoalState);
-					int horizontalDistance = findDistance(y,YPositionInGoalState);;
-					
-					totalDistance = totalDistance + (verticalDistance + horizontalDistance);
-				
-				}
-				
-			}
-		}
-		return totalDistance;
-	}
-	
-	public static int findDistance(int position1, int position2){
-		int distance = Math.abs(position1 - position2);
-		return distance;	
-	}
-	
-	public static int[] findNumber(int[][] board, int number){
-		
-		int[] coordinate = new int[2];
-		
-		for(int y=0;y<3;y++){
-			for(int x=0;x<3;x++){
-				if(board[y][x] == number){
-					coordinate[0] = y;
-					coordinate[1] = x;
-					return coordinate;
-				}
-			}
-		}
-		System.out.println("NUMBER NOT FOUND");
-		return null;
-		
-	}
-	
-	
-	
-	/*NEW way of getting hashKey Code
-	 * This is hardCoded since we are only dealing with 3 x 3
-	 * but can be easily changed into a loop, looks much cleaner/understandable this way
-	 * 
+	/*ex
 	 * if board is 
 	 *  {3,0,2},
 	 *	{6,5,1},
 	 *	{4,7,8}
-	 *Then the ID would be 302651478
+	 *Then the HashCode would be 302651478
 	 *that way no two Boards would have the same ID, unless they are the same board
 	 */
-	@Override
-	public int hashCode(){
-		
-		int id1 = this.start_state[0][0];
-		int id2 = this.start_state[0][1];
-		int id3 = this.start_state[0][2];
-		int id4 = this.start_state[1][0];
-		int id5 = this.start_state[1][1];
-		int id6 = this.start_state[1][2];
-		int id7 = this.start_state[2][0];
-		int id8 = this.start_state[2][1];
-		int id9 = this.start_state[2][2];
-		
-		int id = Integer.valueOf(String.valueOf(id1) + 
-								String.valueOf(id2) +
-								String.valueOf(id3) + 
-								String.valueOf(id4) +
-								String.valueOf(id5) + 
-								String.valueOf(id6) +
-								String.valueOf(id7) + 
-								String.valueOf(id8) +
-								String.valueOf(id9));
-		
-		return id;
-	}
 	
 	
 	@Override
-	public int compareTo(Board otherBoard) {//override, used by binaryHeap to prioritize lower heuristic
+	public int compareTo(Board otherBoard) {//override, used by binaryHeap to prioritize
 		
 		if(this.heuristic<otherBoard.heuristic)
 		{
@@ -252,6 +166,5 @@ public class Board extends HashMap<Integer, Board> implements Comparable<Board>{
 	}
 	
 }
-
 
 
--- Main.java
+++ Main.java
@@ -1,224 +1,145 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
-/* In a test environment
- * A Little bit of refractoring and implementing Manhattan distance heuristic
+/*
+ * Implement hash map to detect duplicate states
+ * ---unsure of what the key to the hash map would be...abandoned that idea
+ * 
+ * Implement Linked Links to detect duplicate states
  */
 
 
-public class Main {
+public class Main extends HashMap<Integer, Board> {
 
-	public static void main(String[] args) {
-		
-		long startTime = System.currentTimeMillis();		
-		
-		
-		int[][] easyBoard = new int[][]{
-			{1,3,4},
-			{8,6,2},
-			{7,0,5}
-		};
-		
-		int[][] mediumBoard = new int[][]{
-			{2,8,1},
-			{0,4,3},
-			{7,6,5}
-		};
-		
-		int[][] hardBoard = new int[][]{
-			{2,8,1},
-			{4,6,3},
-			{0,7,5}
-		};
-		
-		int[][] worstBoard = new int[][]{
-			{5,6,7},
-			{4,0,8},
-			{3,2,1}
-		};
+	public static void main(String[] args){
 		
 		
 		int[][] testBoard = new int[][]{
-			{1,2,3},
-			{8,0,4},
-			{7,6,5}
+			{3,0,2},
+			{6,5,1},
+			{4,7,8}
 		};
 		
-		Board board = new Board(worstBoard);
-		board.parentBoard = null;//this is the root node
-		//board.findMisplacedTiles();
-		int totalNodesVisited = 0;
+		int[][] testBoard2 = new int[][]{
+			{3,0,2},
+			{6,5,1},
+			{4,7,8}
+		};
+
+		int[][] testBoard3 = new int[][]{
+			{3,0,2},
+			{6,5,1},
+			{4,7,8}
+		};
+		
+		System.out.println("TEST");
+
+
+
+		Board board = new Board(testBoard);
+		board.parentBoard = null;
+		board.findMisplacedTiles();
+		
+		Board board2 = new Board(testBoard2);
+		Board board3 = new Board(testBoard3);
+
+
+		/*
+		 * to add to the closed list first we need to get it into <List<List<Integer>> form which will 
+		 * act as a 2d - array, we can't use 2d - array directly because it is not identifiable as a key
+		 * closedList.put(Collections.unmodifiableList(testList), board);<-- key solution found on stack overflow
+		 */
+		HashMap<Integer, Board> closedList = new HashMap<>();
+
+		closedList.put(Arrays.deepHashCode(board.start_state), board);
+		System.out.println(Arrays.deepHashCode(board.start_state));
+		closedList.put(Arrays.deepHashCode(board2.start_state), board2);
+		System.out.println(Arrays.deepHashCode(board2.start_state));
 		
 		
-		BinaryHeap<Board> openList = new BinaryHeap<>();
-		HashMap<Integer, Board> closedList = new HashMap<>();//hashmap closedList
+		System.out.println(closedList.containsKey(Arrays.deepHashCode(board3.start_state)));
+				
 		
-		Queue<Board> fifo = new LinkedList<>();//queue to search by level
-
-
-		String algorithmType = "astar manhattan";
-
-		System.out.println("START");
-		while(totalNodesVisited < 10){//add a way to check if there is any other lower heuristic function
-			/*
-			 * Main if, checks whether the board is already in the closedList
-			 */
-			if(!closedList.containsKey(board.hashCode())){//if there is no same board in hash then proceed
-				
-				closedList.put(board.hashCode(), board);//add the board that we are currently expanding to the closedList
-				System.out.println("|||||||||||||||||||||||||||||");
-				
-				if(Moves.verifyMove("up", board)){
-					System.out.println("Old Board START STATE");
-					board.displayBoard(board.start_state);
-					Board newBoard = new Board(board.start_state);//board with the changes made
-					Moves.move("up", newBoard);
-					System.out.println("New Board 1 START STATE");
-					newBoard.displayBoard(newBoard.start_state);
-					System.out.println("Misplaced tiles = " + newBoard.findMisplacedTiles());
-					newBoard.parentBoard = board;//set the old board as the parent of newBoard
-					System.out.println("Heuristic " + newBoard.calculateHeuristic(algorithmType));
-					addTo(newBoard, openList, fifo, algorithmType);
-					totalNodesVisited++;
+		
+	}
+//	/*NEW way of getting hashKey Code
+//	 * This is hardCoded since we are only dealing with 3 x 3
+//	 * but can be easily changed into a loop, looks much cleaner/understandable this way
+//	 * 
+//	 * if board is 
+//	 *  {3,0,2},
+//	 *	{6,5,1},
+//	 *	{4,7,8}
+//	 *Then the ID would be 302651478
+//	 *that way no two Boards would have the same ID, unless they are the same board
+//	 */
+//	public static int getID(Board board){
+//
+//		int id1 = board.current_state[0][0];
+//		int id2 = board.current_state[0][1];
+//		int id3 = board.current_state[0][2];
+//		int id4 = board.current_state[1][0];
+//		int id5 = board.current_state[1][1];
+//		int id6 = board.current_state[1][2];
+//		int id7 = board.current_state[2][0];
+//		int id8 = board.current_state[2][1];
+//		int id9 = board.current_state[2][2];
+//		
+//		int id = Integer.valueOf(String.valueOf(id1) + 
+//								String.valueOf(id2) +
+//								String.valueOf(id3) + 
+//								String.valueOf(id4) +
+//								String.valueOf(id5) + 
+//								String.valueOf(id6) +
+//								String.valueOf(id7) + 
+//								String.valueOf(id8) +
+//								String.valueOf(id9));
+//		
+//		return id;
+//	}
+//	
+	/*OLD way of having unique HashKey code
+	 * public static void addToClosedList(Board board,  HashMap<List<List<Integer>>, Board> closedList){
+		
+		if(!checkClosedList(board, closedList)){
+			int arrayAsList[] = new int[3];
+			List<List<Integer>> list = new ArrayList<>();
+			
+			for(int y =0; y<Board.boardSizeY;y++){	
+				for(int x=0;x<Board.boardSizeY;x++){
+					arrayAsList[x] = board.start_state[y][x];//store this into arrayAsList so later we can do Arrays.asList()
 				}
-				
-				System.out.println("|||||||||||||||||||||||||||||");
-				
-				if(Moves.verifyMove("down", board)){
-					System.out.println("Old Board START STATE");
-					board.displayBoard(board.start_state);
-					Board newBoard = new Board(board.start_state);//board with the changes made
-					//newBoard.level = board.level +1;
-					Moves.move("down", newBoard);
-					System.out.println("New Board 3 START STATE");
-					newBoard.displayBoard(newBoard.start_state);
-					System.out.println("Misplaced tiles = " + newBoard.findMisplacedTiles());
-					//newBoard.heuristic = newBoard.level + newBoard.misplacedTiles;
-					newBoard.parentBoard = board;
-					System.out.println("Heuristic " + newBoard.calculateHeuristic(algorithmType));
-					addTo(newBoard, openList, fifo, algorithmType);
-
-										
-					totalNodesVisited++;
-
-				}
-				System.out.println("|||||||||||||||||||||||||||||");
-				
-				if(Moves.verifyMove("left", board)){
-					System.out.println("Old Board START STATE");
-					board.displayBoard(board.start_state);
-					Board newBoard = new Board(board.start_state);//board with the changes made
-					//newBoard.level = board.level +1;
-					Moves.move("left", newBoard);
-					System.out.println("New Board 3 START STATE");
-					newBoard.displayBoard(newBoard.start_state);
-					System.out.println("Misplaced tiles = " + newBoard.findMisplacedTiles());
-					//newBoard.heuristic = newBoard.level + newBoard.misplacedTiles;
-
-					newBoard.parentBoard = board;
-					System.out.println("Heuristic " + newBoard.calculateHeuristic(algorithmType));
-					addTo(newBoard, openList, fifo, algorithmType);
-
-
-
-					totalNodesVisited++;
-
-
-				}
-				System.out.println("|||||||||||||||||||||||||||||");
-				
-				if(Moves.verifyMove("right", board)){
-					System.out.println("Old Board START STATE");
-					board.displayBoard(board.start_state);
-					Board newBoard = new Board(board.start_state);//board with the changes made
-					//newBoard.level = board.level +1;
-					Moves.move("right", newBoard);
-					System.out.println("New Board 3 START STATE");
-					newBoard.displayBoard(newBoard.start_state);
-					System.out.println("Misplaced tiles = " + newBoard.findMisplacedTiles());
-					//newBoard.heuristic = newBoard.level + newBoard.misplacedTiles;
-					newBoard.parentBoard = board;
-					System.out.println("Heuristic " + newBoard.calculateHeuristic(algorithmType));
-					addTo(newBoard, openList, fifo, algorithmType);
-
-
-
-					totalNodesVisited++;
-
-				}
-				System.out.println("|||||||||||||||||||||||||||||");
+				list.add(Arrays.asList(arrayAsList[0],arrayAsList[1],arrayAsList[2]));
 			}
-			
-			board = removeFrom(board, openList, fifo, algorithmType);
-			board.findBlankPos();
-			int boardLevel = board.level;
-			System.out.println("BOARD LEVEL " + boardLevel);
-			
-			System.out.println("--------------------------------");
+			closedList.put(Collections.unmodifiableList(list), board);//list now can be used as a key in the hashMap
+		}else{
+			System.out.println("{{{{{{{There is already the same board added}}}}}}}");
 		}
-
-		
-		
-		System.out.println("ANSWER BOARD");
-		
-		Board currentState = Board.answerBoard;
-		int nodesVisited = 0;
-		//displays the path from start state to goal state
-		while(currentState != null){
-			currentState.displayBoard(currentState.start_state);
-			currentState  = currentState.parentBoard;
-			nodesVisited++;
-			System.out.println();
-		}
-		nodesVisited--;
-		
-		System.out.println("Optimal Solution Nodes: "+nodesVisited);
-		
-		long endTime   = System.currentTimeMillis();
-		long totalTime = endTime - startTime;
-		System.out.println("Total Time: "+totalTime);//6760 = 6.7s
-		System.out.println("Total Nodes Visited: "+totalNodesVisited);//6760 = 6.7s
-		System.out.println("Total Nodes Expanded: "+closedList.size());//6760 = 6.7s
-		
-	}	
+	}
 	
-	/*
-	 * Adds the board to its appropriate data structure 
-	 * If it is astar heuristic(Misplacement) or astar manhattan 
-	 * it is added to priority Queue because lower the priority the better
-	 * 
-	 * But if it is iterative deepening search then it is added to a FIFO queue
-	 * where it searches by the depth/level
+	
+	 // Check if a specific board is in the closed list
+	 //return true if it is
+	
+	public static boolean checkClosedList(Board board, HashMap<List<List<Integer>>, Board> closedList){
+		int arrayAsList[] = new int[3];//so we can do Arrays.asList(...) with the array as one of the rows of the board
+		List<List<Integer>> list = new ArrayList<>();
+		
+		for(int y =0; y<Board.boardSizeY;y++){	
+			for(int x=0;x<Board.boardSizeY;x++){
+				arrayAsList[x] = board.start_state[y][x];
+			}
+			list.add(Arrays.asList(arrayAsList[0],arrayAsList[1],arrayAsList[2]));//adds the array value to the list
+		}
+		return closedList.containsKey(list);//returns true of there is already the same board in hash
+	}
 	 */
-	public static void addTo(Board board, BinaryHeap<Board> openList, Queue<Board> fifo, String algorithm){
-		algorithm.toLowerCase();
-		
-		if(algorithm == "astar misplaced" || algorithm == "astar manhattan"){
-			openList.insert(board);
-		}
-		if(algorithm == "astar ida"){
-			fifo.add(board);
-		}
-	}
 	
-	public static Board removeFrom(Board board, BinaryHeap<Board> openList, Queue<Board> fifo, String algorithm){
-		algorithm.toLowerCase();
-		
-		if(algorithm == "astar misplaced" || algorithm == "astar manhattan" && !openList.isEmpty()){
-			 return openList.deleteMinimum();
-		}
-		if(algorithm == "astar ida"){
-			Board depthBoard = fifo.remove();
-			return depthBoard;
-		}
-		
-		return board;
-	}
+	
 
 }
